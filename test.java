e589106da1429d8311b54a232688f8e437c22045
gab50000 <fake@xyz.de>
2013-06-11 02:13:59 +0200

    xyzFile object works, acidic Hs not yet added

--- test.java
+++ test.java
@@ -1,29 +1,52 @@
+import java.util.*;
 public class test{
 
 	public static void main(String[] args){
 		Atom a = new Atom("H", 1, 2, 3);
 		Atom b = new Atom("H", 0, 0, 0);
 
-		System.out.println(a.dist(b));
+		//~ System.out.println(a.dist(b));
 
 		try{
 			xyzFile hex400 = new xyzFile("../../Master/trajectories/400K.xyz");
-			
-			Atom[] atoms = hex400.getAtoms();
-			int j;
-			System.out.println(atoms.length);
-			for (int i = 0; i < atoms.length; i++){
-				for (j = 0; j < i; j++){
-					System.out.println(atoms[i].dist(atoms[j]));
-				}
+			String[] atoms={"P", "O"};
+			hex400.setAtomsShown(atoms);
+			for (int i = 0; i<75000; i++){
+				hex400.readFrame();
 			}
-
-
-		}
+			}
 		catch (Exception e){
-			System.out.println("Nö");
+			System.err.println("Nö");
 		}
 
+
+		//~ String str= "    1 2 3   5 6  7";
+//~ 
+		//~ String[] str2=str.replaceAll("^\\s+","").split("\\s+");
+//~ 
+		//~ System.out.println(str2[0]);
+//~ 
+		//~ Map<String,Vector<Integer>> m = new HashMap<>();
+//~ 
+		//~ m.put("H", new Vector<Integer>());
+		//~ m.get("H").add(1);
+//~ 
+		//~ m.put("O", new Vector<Integer>());
+		//~ m.get("O").add(3);
+//~ 
+		//~ System.out.println(m.containsKey("H"));
+		//~ 
+		//~ System.out.println(m);
+//~ 
+		//~ m.get("O").add(4);
+//~ 
+		//~ System.out.println(m);
+		//~ 
+		//~ ArrayList<Integer> abc = new ArrayList<>();
+		//~ abc.add(1);
+		//~ abc.add(3);
+		//~ abc.add(12);
+		//~ System.out.println(abc);
 
 	}
 }
--- xyzFile.java
+++ xyzFile.java
@@ -3,23 +3,59 @@ import java.util.*;
 public class xyzFile{
 	
 	private Scanner sc;
-	private int atomnumber;
-	private Map indexmap;
+	private int atomnumber, selectedatomnumber, framecount=0;
+	private HashMap <String, ArrayList<Integer>> indexmap;
+	private boolean[] indicestoshow;
+	private ArrayList<Integer> Hacid_indices;
 	
 	public xyzFile(String filename) throws FileNotFoundException{
 		this.sc = new Scanner(new File(filename));
 		this.atomnumber = sc.nextInt();
+		sc.nextLine();
+		sc.nextLine();
+
+		this.indicestoshow=new boolean[this.atomnumber];
+		for (int i=0; i<this.atomnumber; i++){
+			this.indicestoshow[i]=true;
+		}
+		this.indexmap = determineAtoms(sc, atomnumber);
 		this.sc.close();
 		this.sc = new Scanner(new File(filename));
 	}
 
+	private HashMap<String,ArrayList<Integer>> determineAtoms(Scanner sc, int steps){
+		String[] line;
+		HashMap <String, ArrayList<Integer>> indexmap = new HashMap<>();
+		for (int i = 0; i < steps; i++){
+			line=sc.nextLine().replaceAll("^\\s+", "").split("\\s+");
+			if (indexmap.containsKey(line[0])){
+				indexmap.get(line[0]).add(i);
+			}
+			else{
+				indexmap.put(line[0], new ArrayList<Integer>());
+				indexmap.get(line[0]).add(i);
+			}
+		}
+		
+		return indexmap;
+	}
+
 	public void readFrame() throws IOException{
+		System.out.println(this.selectedatomnumber);
+		System.out.println();
+		
 		sc.nextLine();
 		sc.nextLine();
 
 		for (int i = 0; i < this.atomnumber; i++){
-			System.out.println(sc.nextLine());
+			if (this.indicestoshow[i]){
+				System.out.println(sc.nextLine());
+			}
+			else{
+				sc.nextLine();
+			}
 		}
+		framecount++;
 	}
 
 	public Atom[] getAtoms() throws IOException{
@@ -27,14 +63,60 @@ public class xyzFile{
 		sc.nextLine();
 
 		Atom [] atoms = new Atom[this.atomnumber];
+		String[] line;
 
 		for (int i = 0; i < this.atomnumber; i++){
-			String[] line=sc.nextLine().replaceAll("^\\s+", "").split("\\s+");
-			System.out.println(line[0]+";"+line[1]+";"+line[2]+";"+line[3]);
-			atoms[i] = new Atom(line[0], Double.parseDouble(line[1]), Double.parseDouble(line[2]), Double.parseDouble(line[3]));
+			if (this.indicestoshow[i]){
+				line=sc.nextLine().replaceAll("^\\s+", "").split("\\s+");
+				atoms[i] = new Atom(line[0], Double.parseDouble(line[1]), Double.parseDouble(line[2]), Double.parseDouble(line[3]));
+			}
+			else{
+				sc.nextLine();
+			}
 		}
+		framecount++;
 
 		return atoms;
 	}
+
+	public void setAtomsShown(String[] atoms){
+
+		ArrayList<Integer> atomstoshow = new ArrayList<Integer>();
+		for (int i = 0; i < atoms.length; i++){
+			if (this.indexmap.containsKey(atoms[i])){
+				atomstoshow.addAll(this.indexmap.get(atoms[i]));
+			}
+		}
+		
+		Collections.sort(atomstoshow);
+		for (int i = 0; i < this.atomnumber; i++){
+			if (atomstoshow.contains(i)){
+				this.indicestoshow[i] = true;
+			}
+			else{
+				 this.indicestoshow[i] = false;
+			 }
+		 }
+		 
+		 this.selectedatomnumber = atomstoshow.size();
+		 
+	}
+	
+	public Set<String> getAtomTypes(){
+		return this.indexmap.keySet();
+	}
+	
+	public int getAtomCount(String name){
+		if (this.indexmap.containsKey(name)){
+			return this.indexmap.get(name).size();
+		}
+		else return 0;
+	}
+	
+	public set_acidicHs(){
+		Atom[] Hs = 
+	}
+	
+	
 }
 
